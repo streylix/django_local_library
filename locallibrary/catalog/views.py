761efd0b451911c65e25e2702d147d328cc4f34e
Ethan Pae <paeem@dukes.jmu.edu>
2025-02-24 12:04:51 -0500

    part 6

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -28,3 +29,24 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.all() # Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'author_list'   # your own name for the list as a template variable
+    queryset = Author.objects.all() # Get 5 books containing the title war
+    template_name = 'authors/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
